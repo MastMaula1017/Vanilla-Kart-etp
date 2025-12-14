f2eaa620c5af1870630f8abe9c4f5daf314710a3
Vansh Raturi <riderknight6789@gmail.com>
2025-12-14 14:38:20 +0530

    chore: remove debug info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,10 @@ import ProfilePage from './pages/ProfilePage';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import Footer from './components/Footer';
-import DebugInfo from './components/DebugInfo';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-950 dark:text-gray-100 transition-colors duration-300 flex flex-col">
-      <DebugInfo />
       <Navbar />
       <div className="container mx-auto px-4 py-8 flex-grow">
         <Routes>
