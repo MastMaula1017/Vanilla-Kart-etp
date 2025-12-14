59abf81772fc9cfe4a9a055ce4982ad287309458
Vansh Raturi <riderknight6789@gmail.com>
2025-12-15 00:19:56 +0530

    testing forgot in p

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,9 +4,7 @@ const sendEmail = async (options) => {
   // Create transporter
   // START WITH MAILTRAP / GENERIC SMTP CONFIG
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -15,7 +13,7 @@ const sendEmail = async (options) => {
 
   // Define email options
   const mailOptions = {
-    from: process.env.EMAIL_FROM || '"Support Team" <noreply@example.com>',
+    from: process.env.EMAIL_FROM || process.env.EMAIL_USER, // Use custom name or default to user email
     to: options.email,
     subject: options.subject,
     text: options.message,
