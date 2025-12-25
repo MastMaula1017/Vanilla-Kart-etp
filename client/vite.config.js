1ff8f12dfa1a31e2bfebe5000ea74b4223b57b75
Vansh Raturi <riderknight6789@gmail.com>
2025-12-25 12:54:08 +0530

    fixed bug of PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,6 +17,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
+        start_url: '/',
+        orientation: 'portrait',
         icons: [
           {
             src: 'favicon.png',
