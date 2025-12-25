fcf9522e1e816f5805fed52250bf3bb5ed6fabc6
Vansh Raturi <riderknight6789@gmail.com>
2025-12-25 12:47:05 +0530

    Added PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'favicon.svg'],
+      manifest: {
+        name: 'ConsultPro',
+        short_name: 'ConsultPro',
+        description: 'Expert Consultation Platform',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     include: ['gsap'],
   },
