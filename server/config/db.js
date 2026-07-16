188bac02e6e62dc300715b75d1ab7f0d400cd9f9
MastMaula1017 <vansh.12320821@lpu.in>
2026-07-16 20:12:07 +0530

    Log safe database host to help debug Heroku connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    const mongoUri = process.env.MONGO_URI || '';
+    const host = mongoUri.split('@')[1] || 'unknown host';
+    console.log(`Attempting to connect to database host: ${host}`);
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
